779aea12cb1a1ffe94f4ed38574f03baa95c6fe6
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2021-11-29 13:58:37 +0000

    sampling 12 months rndom climatology for each sample. Fixig bug in posterior sampling

--- optimise_run_model/make_model_output.py
+++ optimise_run_model/make_model_output.py
@@ -19,7 +19,13 @@ from   libs.plot_maps    import *
 from scipy.stats import norm
 from scipy.stats import skewnorm
 from scipy.stats import lognorm
+import random
 from pdb import set_trace as browser
+
+
+def mkDir(dir):
+    try: os.mkdir(dir)  
+    except: pass
 
 def npLogit(x):
     return np.log(x/(1.0-x))
@@ -61,11 +67,13 @@ def newCubes3D(variable, step, eg_cube_in, dimname = 'model_level_number', minV 
 ## input info ##
 ################
 
-dir = "../data/ISIMIP_data2/"
-model_names = os.listdir(dir)[0:4]
-experiments = os.listdir(dir + model_names[0])
+dir = "../ConFIRE_ISIMIP/inputs2/"
+model_names = os.listdir(dir)
 
-
+model_names = [model_names[2]]
+experiments = os.listdir(dir + model_names[0])#[0:2]
+browser()
+#experiments = [experiments[0]]
 files = {'soilwMax'           : 'soil12.nc',
          'shallow_soilw'      : 'soilM_top.nc',
          'deep_soilw'         : 'soilM_bottom.nc',
@@ -75,9 +83,9 @@ files = {'soilwMax'           : 'soil12.nc',
          'pasture'            : 'pas.nc',
          'cropland'           : 'crop.nc',
          'vegcover'           : 'totalVeg.nc',
-         'burnt_area'         : '../../burnt_area_GFED4sObs.nc'}
+         'burnt_area'         : '../../../burnt_area_GFED4sObs.nc'}
 
-param_file = '../outputs/params-for_sampling/'
+param_file = '../ConFIRE_ISIMIP/outputs/params-for_sampling/'
 title_output = 'attempt4-full'
 
 ###############
@@ -88,7 +96,10 @@ def loadInputsParams(model, experiment):
     print(experiment)
     input_data = {}
     for key, file in files.items():
-        data = iris.load_cube(dir + model + '/' + experiment + '/' + file)
+        try:
+            data = iris.load_cube(dir + model + '/' + experiment + '/' + file)
+        except:
+            browser()
         input_data[key] = data
 
     params = pd.read_csv(param_file + model + '.csv')
@@ -177,6 +188,7 @@ class ConFIRE(object):
             self.burnt_area_mode.long_name = "burnt_area_mode"
             #self.burnt_area_median.long_name = "burnt_area_median"
             self.burnt_area_mean.long_name = "burnt_area_mean"
+            self.burnt_area_pdf.long_name = "burnt_area_pdf"        
 
             self.fuel.long_name = "fuel continuity"
             self.fuel.units = '1'
@@ -349,7 +361,6 @@ def bootSamples(x, samples, nsample = 2):
     def bootSample(x):
         #browser()or np.ma.is_masked(test)
         test = np.ma.sum(x)
-        
         out = np.repeat(0, nsample)
         if test==0 : return out
         try:
@@ -357,47 +368,46 @@ def bootSamples(x, samples, nsample = 2):
         except:
             out = np.repeat(0, nsample)
         return out
+   
     x = np.apply_along_axis(bootSample, 0, x)
+    
     return(x)
 
 
 
-
-def bootSamples(x, samples, nsample = 2):
-    def bootSample(x):
-        #browser()or np.ma.is_masked(test)
-        test = np.ma.sum(x)
-        
-        out = np.repeat(0, nsample)
-        if test==0 : return out
-        try:
-            out = np.random.choice(samples, nsample, p =x/np.sum(x))
-        except:
-            out = np.repeat(0, nsample)
-        return out
-    x = np.apply_along_axis(bootSample, 0, x)
-    return(x)
-
-
-
-def runModelExperiment(input, paramLoc = None, nmns = None):
+def runModelExperiment(input, paramLoc = None, nmns = None, nyr = None):
     input_data = input[0].copy()
     params = input[1]
     if paramLoc is None: paramLoc = params["sigma"].idxmin()
     print("running")
-    browser()
-    if nmns is None: nmns = input[0]["precip"].shape[0]
-    for mn in range(0, nmns):
+    
+    if nyr is None:
+        mns = range(0, input[0]["precip"].shape[0])
+    else:
+        mns = np.random.choice(np.int(input[0]["precip"].shape[0]/12), 12*nyr)*12
+        mns += (np.arange(0, 12, 1/nyr)*nyr).astype(int)
+        mns = np.sort(mns)
+    
+    if nmns is not None and nmns < len(mns): mns = mns[0:nmns]
+    
+    nmns = len(mns)
+    
+    for mn in mns:
+        print(mn)
         print(mn/nmns) 
         for key in input_data.keys():
             try:
                 input_data[key] = input[0][key][mn]
             except:
                 #print(key)
-                input_data[key] = input[0][key][mn-12]
+                try:
+                    input_data[key] = input[0][key][mn-12]
+                except:
+                    browser()
         model = ConFIRE(input_data, params.loc[paramLoc])
+        
         PDF = model.burnt_area_pdf.data
-        if mn == 0:
+        if mn == mns[0]:
             mask = PDF[0,:,:].mask == False
             BAs = model.burnt_area_pdf.coord("model_level_number").points
             BAs = 1/(1+np.exp(-BAs))
@@ -405,7 +415,7 @@ def runModelExperiment(input, paramLoc = None, nmns = None):
             
         PDF = bootSamples(PDF[:,mask], BAs, 10)
         
-        if mn == 0:
+        if mn == mns[0]:
             model_out = model
             PDF_out = PDF
         else:
@@ -426,7 +436,7 @@ def runModelExperiment(input, paramLoc = None, nmns = None):
             model_out.potential_suppression.data += model.potential_suppression.data 
             
             model_out.sensitivity_fuel.data += model.sensitivity_fuel.data 
-            model_out.sensitivity_moisture .data += model.sensitivity_moisture.data 
+            model_out.sensitivity_moisture.data += model.sensitivity_moisture.data 
             model_out.sensitivity_ignitions.data += model.sensitivity_ignitions.data 
             model_out.sensitivity_suppression.data += model.sensitivity_suppression.data 
             PDF_out += PDF
@@ -450,30 +460,41 @@ def runModelExperiment(input, paramLoc = None, nmns = None):
     model_out.potential_suppression.data /= nyrs
 
     model_out.sensitivity_fuel.data /= nyrs
-    model_out.sensitivity_moisture /= nyrs
+    model_out.sensitivity_moisture.data /= nyrs
     model_out.sensitivity_ignitions.data /= nyrs
     model_out.sensitivity_suppression.data  /= nyrs
-    PDF_out /= nmns
     
+    PDF_out /= nmns
     model_out.burnt_area_pdf.data[:] = 0.0
     
     def posterize(i): return np.argmin(np.abs(i-BAs))
     vpost = np.vectorize(posterize)
+    PDF_out0 = PDF_out.copy()
     PDF_out = vpost(PDF_out)
+   
     dummy = PDF_out[0].copy()
-    dummy[:] = 0.0
+    
     for PDF in PDF_out:
         for i in np.unique(PDF):
+            dummy[:] = 0.0
             dummy[PDF == i] = 1.0
             try:
-                model_out.burnt_area_pdf.data[i][mask] =+ dummy
+                model_out.burnt_area_pdf.data[i][mask] += dummy 
             except:
-                browser()   
+                browser()  
+    
     return model_out
+
+output_dir = '../ConFIRE_ISIMIP/outputs/sampled_posterior_ConFire_ISIMIP_solutions/'
+mkDir(output_dir)
+
 #models = [[runModelExperiment(input) for input in i] for i in inputs]
+#model_example_file = output_dir + 'example_Confire.nc'
 model = runModelExperiment(inputs[0][0], nmns = 2)
+#browser()
+#iris.save(model, model_example_file)
      
-browser()
+
 ##########################
 ## perform boottrapping ##
 ##########################
@@ -484,9 +505,7 @@ output_sensitivity = True
 output_fullPost = True
 
 n_posterior_sample = 10
-
 qs = np.arange(1, 100, 1)
-output_dir = '../outputs/sampled_posterior_ConFire_ISIMIP_solutions/'
 
 BAs = model.burnt_area_pdf.coord("model_level_number").points
 BAs = 1/(1+np.exp(-BAs))
@@ -505,23 +524,18 @@ def weighted_percentile(data, percents, weights=None):
     y=np.interp(percents, p, d)
     return y
 
-
-def mkDir(dir):
-    try: os.mkdir(dir)  
-    except: pass
-
-
-mkDir(output_dir)
-
 output_dir = output_dir + title_output +'/'
 mkDir(output_dir)
-
-
 
 def bootModel(input, model, experiment):
     output_diri = output_dir + '/' + model
     mkDir(output_diri)
     output_diri = output_diri + '/' + experiment + '/'
+    
+    outFileSumm = output_diri + 'model_summary-' + str(n_posterior_sample) + '.nc'
+    outFilePost = output_diri + 'fullPost-' + str(n_posterior_sample) + '.nc'
+
+    if os.path.isfile(outFilePost): return()
     mkDir(output_diri)
     fire_outi = []
     n_posterior = input[1].shape[0]
@@ -529,45 +543,55 @@ def bootModel(input, model, experiment):
     if ngap == 0: ngap = 1
     
     for i in range(0, n_posterior, ngap):
-        model = runModelExperiment(input, i, nmns = 24)        
+        outFile = output_diri + 'sample_no_' + str(i) +'.nc'
+        if os.path.isfile(outFile):
+            cubes = iris.load(outFile)
+        else:          
+            model = runModelExperiment(input, i, nyr = 1)        
+            
+            #model = ConFIRE(input_data, params.iloc[i])
+            burnt_area = model.burnt_area_mean
+            
+            cubes = [model.burnt_area_mean]
+            if output_fullPost:
+                cubes = cubes + [model.burnt_area_pdf]
+            
+            if output_controls:
+                cubes = cubes + [model.fuel, model.moisture, 
+                                 model.ignitions, model.suppression]
+            
+            if output_standard_limitation:
+                cubes = cubes + [model.standard_fuel, model.standard_moisture, 
+                                 model.standard_ignitions, model.standard_suppression]
+            
+            if output_potential_limitation:
+                cubes = cubes + [model.potential_fuel, model.potential_moisture,                
+                                 model.potential_ignitions, model.potential_suppression]
+
+            if output_sensitivity:
+                cubes = cubes + [model.sensitivity_fuel, model.sensitivity_moisture,            
+                                 model.sensitivity_ignitions, model.sensitivity_suppression]
+            
+            cubes = iris.cube.CubeList(cubes)             
+            
+            iris.save(cubes, outFile)
+        nmes = [cube.name() for cube in cubes]   
+        pdfID = np.where(np.array(nmes) == "burnt_area_pdf")[0][0]     
+        MPDF = cubes[pdfID].copy()
+        del cubes[pdfID]
+        nmes = [cube.name() for cube in cubes]   
+        cubes = iris.cube.CubeList([x for y, x in sorted(zip(nmes, cubes))])
         
-        #model = ConFIRE(input_data, params.iloc[i])
-        burnt_area = model.burnt_area_mean
-        
-        cubes = [model.burnt_area_mean]
         if output_fullPost:
             if i == 0:
-                fullPost = model.burnt_area_pdf
+                fullPost = MPDF.copy()
             else:
-                fullPost = fullPost + model.burnt_area_pdf   
-
-        if output_controls:
-            cubes = cubes + [model.fuel, model.moisture, 
-                             model.ignitions, model.suppression]
-
-        if output_standard_limitation:
-            cubes = cubes + [model.standard_fuel, model.standard_moisture, 
-                             model.standard_ignitions, model.standard_suppression]
-
-        if output_potential_limitation:
-            cubes = cubes + [model.potential_fuel, model.potential_moisture,                 
-                             model.potential_ignitions, model.potential_suppression]
-
-        if output_sensitivity:
-            cubes = cubes + [model.sensitivity_fuel, model.sensitivity_moisture,                  
-                             model.sensitivity_ignitions, model.sensitivity_suppression]
-
-        cubes = iris.cube.CubeList(cubes) 
-        if i == 0:
-            fullPDF = model.burnt_area_pdf.copy()
-        else:
-            fullPDF.data =+ model.burnt_area_pdf.data
+                fullPost.data =+ MPDF.data
         
         fire_outi = fire_outi + [cubes]
-        outFile = output_diri + 'sample_no_' + str(i) +'.nc'
+        
         print(outFile)
-        iris.save(cubes, outFile)
-      
+    
     fire_out = []
     for i in range(len(fire_outi[0])):
         #print(i)
@@ -580,19 +604,16 @@ def bootModel(input, model, experiment):
 
         fire_out = fire_out + [percentile_cube]
 
-    outFile = output_diri + 'model_summary.nc'
     fire_out = iris.cube.CubeList(fire_out) 
-    print(outFile)
-    iris.save(fire_out, outFile)    
+    print(outFileSumm)
+    iris.save(fire_out, outFileSumm)    
     
-    outFile = output_diri + 'fullPost.nc'
-    print(outFile)
-    browser()
-    iris.save(fullPost, outFile) 
+    print(outFilePost)
+    iris.save(fullPost, outFilePost) 
         
-#for model, i in zip(model_names, inputs):
-model = model_names[3]
-i = inputs[3]
-for experiment, input in zip(experiments, i):
-    print(model)
-    bootModel(input, model, experiment)
+for model, i in zip(model_names, inputs):
+#model = model_names[3]
+#i = inputs[3]
+    for experiment, input in zip(experiments, i):
+        print(model)
+        bootModel(input, model, experiment)
