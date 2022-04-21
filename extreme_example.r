0984e13f8c414156d44db1fe486d33af2c3e9b12
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2022-04-21 10:47:04 +0100

    prettier extreme exam ple

--- extreme_example.r
+++ extreme_example.r
@@ -7,9 +7,10 @@ sourceAllLibs("../rasterextrafuns/rasterExtras/R/")
 sourceAllLibs("libs/")
 
 
-x = seq(0, 1, by = 0.001)
+x = seq(0.1, 0.8, by = 0.0005)
 
 xl = logit(x)
+x = (x-0.1)*30 
 
 muH = logit(0.3)
 sdH = 0.6
@@ -19,31 +20,52 @@ sdF = 0.7
 
 distr <- function(mu, sd) {
     y = dnorm(xl, mu, sd)
+    y = y + dnorm(xl, -2.19, 0.3) * 0.3
     y = y/max(y)        
     cy = pnorm(xl, mu, sd)
     selectXatT <- function(thresh) 
         x[which(cy > thresh)[1]]
    
-    list(y, selectXatT(0.05), selectXatT(0.95), selectXatT(0.99), selectXatT(0.995))
+    list(y, selectXatT(0.1), selectXatT(0.5), selectXatT(0.9), selectXatT(0.99))
 }
 
 yH = distr(muH, sdH)
 yF = distr(muF, sdF)
 
-png("figs/BAchange_extreme_example.png", width = 7, height = 7, units = 'in', res = 300)
-plot(c(0, 1), c(0, 1.3), type = 'n', axes = FALSE, xlab = '', ylab = '')
+png("figs/BAchange_extreme_example.png", width = 7.5*0.9, height = 5*0.9, units = 'in', res = 300)
+plot(range(x), c(0, 1.05), type = 'n', axes = FALSE, xlab = '', ylab = '', xaxs = 'i', yaxs = 'i')
+lines(rep(min(x), 2),c(-100, 100))
+#axis(1)
+mtext(side = 1, line = 0.5, "Burnt Area (%)", font = 2)
+mtext(side = 2, line = 0.5, "Probability of occurance", font = 2)
 
-addPoly <- function(xp, y, col, border= col) 
-    polygon(xp, y[[1]], col = make.transparent(col, 0.9), border = border)
-
-addPolyLines <- function(y, col, pos, txt) {
-    addPoly(x, y, col)
-    lapply(y[2:3], function(x) lines(rep(x, 2), c(0, pos), lty = 2, col = col))
-    lapply(y[2:3], function(x) lines(rep(x, 2), pos + c(-0.05, 0.05), col = col))
-    lines(unlist(y[2:3]), rep(pos, 2), col = col)
-    text(x = mean(unlist(y[2:3])), y = pos+0.05, txt, col = col)
+addBox <- function(H, col, txt) {
+    polygon(H[c(1, 2, 2, 1)], c(0, 0, 1.1, 1.1), border = NA, col = make.transparent(col, 0.0))
+    polygon(H[c(1, 2, 2, 1)], c(0, 0, 1.1, 1.1), border = NA, col = make.transparent("white", 0.2))
+    #lines(H[c(2,2)], c(0, 1), lty = 3, lwd = 2)
+    text(x = mean(H), y = 0.9, txt)
 }
-#for (i in 1:3) {
+
+cols = rev(c('#d7191c','#fdae61','#ffffbf','#abd9e9','#2c7bb6'))
+test = x > yH[[5]]
+
+addBox(c(min(x), yH[[2]]), cols[1], "No\nFire")
+addBox(c(yH[[5]], max(x)), cols[5], "Wildfire")
+
+pFUN <- function(y, ...)
+    polygon(c(x[test][1], x[test], tail(x[test], 1)), c(0, y[[1]][test], 0), col = tail(cols, 1), border = NA, ...)
+pFUN(yH)
+pFUN(yF, density = 10, lwd = 4, lend = 1, angle = 60)
+
+for (i in 2:4) 
+    addBox(c(yH[[i]], yH[[i+1]]), cols[i], c("", "", "High Fire\nActivity")[i-1])
+lines(x, yH[[1]], lwd = 4)
+lines(x, yF[[1]], lwd = 4, lty = 2)
+
+text(10, 0.53, srt = -50, 'Historic (2010s)')
+text(12, 0.64, srt = -45, 'Future')
+dev.off()
+browser()
 addPolyLines(yH, "blue", 1, txt = 'Historic range')
 addPolyLines(yF, "red", 1.1, txt = 'Future range')
 #}
