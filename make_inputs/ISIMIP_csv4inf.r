caee814aae34bec381dbc592b139cb22c14a6ad4
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2022-08-26 09:59:01 +0100

    updating path and correctingtemp fuilename

--- make_inputs/ISIMIP_csv4inf.r
+++ make_inputs/ISIMIP_csv4inf.r
@@ -2,7 +2,7 @@ library(raster)
 source("../gitProjectExtras/gitBasedProjects/R/sourceAllLibs.r")
 sourceAllLibs("../rasterextrafuns/rasterExtras/R/")
 
-dir = '/data/users/dkelley/ConFIRE_ISIMIP/isimip3_inputs/Global/historic_TS_short/obsclim/'
+dir = '/data/users/dkelley/ConFIRE_ISIMIP/isimip3_inputs/GSWP3-W5E5/Global/historic_TS_2000_2019/obsclim/'
 temp_dir = '/data/users/dkelley/ConFIRE_ISIMIP_temp/ISIMIP_csv4ins'
 out_file = "/data/users/dkelley/ConFIRE_ISIMIP/isimip3_inputs/Global/inference_data/GSWP5.csv"
 
@@ -28,10 +28,12 @@ extract <- function(dat, name) {
     extractLayer <- function(i) dat[[i]][mask]
     
     print(name)
-    tfile = paste(temp_dir, '.Rd', sep = '-')
+    tfile = paste(temp_dir, name, '.Rd', sep = '-')
+    
     if (file.exists(tfile)) {load(tfile); return(mat)}
     mat = sapply(1:nlayers(dat), extractLayer)
     mat = as.vector(mat)
+    
     save(mat, file = tfile)
     return(mat)
 }
