626649d06aaf41b5d87705e1a9ef334ef489bbd4
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2022-08-28 08:33:24 +0100

    setting up to run none-boostraps (which dont have extra concatinate dimenion) and allowing annual summery outputs

--- ConFire/run.py
+++ ConFire/run.py
@@ -11,6 +11,11 @@ import pandas as pd
 import csv
 
 import iris
+import iris.coords
+from iris.coords import DimCoord
+import iris.coord_categorisation as cat
+import cf_units
+
 import matplotlib.pyplot as plt
 import numpy.ma as ma
 import cartopy.crs as ccrs
@@ -30,21 +35,22 @@ import iris.plot as iplt
 import iris.quickplot as qplt
 import matplotlib.pyplot as plt
 
-input_dir = "/data/users/dkelley/ConFIRE_ISIMIP/isimip3_inputs/GSWP3-W5E5/Global/"
-output_dir = "/data/users/dkelley/ConFIRE_ISIMIP/isimip3a_outputs/"
+input_dir = "isimip3a/driving_data/GSWP3-W5E5/Global/"
+output_dir = "isimip3a/outputs/test/"
 identfier = "historic_TS"
 experiments = ["obsclim/", "counterclim/"]
 
-param_file = "../ConFIRE_ISIMIP/outputs/isimip3/params-for_sampling/GSWP5.csv"
+param_file = "isimip3a/driving_data/GSWP3-W5E5/params-for_sampling/with_ancils-GSWP5.csv-2-1.csv"
 periods = fnmatch.filter(os.listdir(input_dir), 'hist*')
 periods = sorted(periods, reverse = True)
 
 output = ["controls", "standard", "potential", "sensitivity", "Bootstraps"]
 output = ["controls", "standard", "Bootstraps"]
 
-n_posterior_sample = 4
+n_posterior_sample = 10
+nBootstrap = 10
 
-qs = np.array([0.01, 0.05, 0.1, 0.5, 0.9, 0.95, 0.99])
+qs = np.array([1, 5, 10, 25, 50, 75, 90, 95, 99])
 
 ignore_lock = False
 
@@ -72,7 +78,7 @@ def runSample(paramLoc, output_dir, params, input_data):
     lock_file = output_dir_sample + 'lock.txt'
     if ignore_lock or not os.path.exists(lock_file):
         model = ConFire(input_data, params.loc[paramLoc], 
-                        run_potential = run_potential, run_sensitivity = run_sensitivity, nBootstrap = 5)
+                        run_potential = run_potential, run_sensitivity = run_sensitivity, nBootstrap = nBootstrap)
         
         makeSampleOutputFile(model.burnt_area_mode, output_dir_sample, 'burnt_area_mode')
         if np.any(np.array(output) == 'controls'):
@@ -108,30 +114,51 @@ def runSample(paramLoc, output_dir, params, input_data):
 def listFiles_recursive(PATH, fname = '', ext = '.nc'):
     return([os.path.join(dp, f) for dp, dn, filenames in os.walk(PATH) for f in filenames if fname in f and os.path.splitext(f)[1] == ext])
 
+def npLogistic(x):
+    return(1/(1+np.exp(-x)))
 
-
-def build_distribution_from_boots(ensembles_dir, output_dir, var, period):
-    output_dir_var = output_dir + var + '/'
+def build_distribution_from_boots(ensembles_dir, output_dir, var, period, output_period = "monthly", transform = None):
+    ## setup directories and path
+    if var == 'bootstrap_': output_dir_var = output_dir + "burnt_area_full_posterior"
+    else: output_dir_var = output_dir + var
+    output_dir_var = output_dir_var + '-' + output_period + '/'
     mkDir(output_dir_var)
     outFile = output_dir_var + period + '.nc'
+
     if ignore_lock or not os.path.exists(outFile):
+        ## open and orgaise cube
         files = listFiles_recursive(ensembles_dir, var)
         cubes = iris.load(files)
+        
+        try:
+            mls = np.arange(0.0, len(cubes[1].coord('model_level_number').points))
+            for cube in cubes:  
+                cube.coord('model_level_number').points = mls
+                mls = mls + len(cubes[1].coord('model_level_number').points)
+            cubes = cubes.concatenate()[0]
+        except:
+            for i in range(len(cubes)):
+                coord = DimCoord(np.arange(1) + i, "model_level_number")
+                cubes[i].add_aux_coord(coord)
+            cubes = cubes.merge()[0]
+        
+        if transform is not None: 
+            ifunc = iris.analysis.maths.IFunc(transform, lambda cube: cf_units.Unit('1'))
+            cubes = ifunc(cubes)
+        if output_period == "annual": 
+            iris.coord_categorisation.add_year(cubes, 'time')
+            cubes = cubes.aggregated_by('year',  iris.analysis.MEAN)
     
-        mls = np.arange(0.0, len(cubes[1].coord('model_level_number').points))
-    
-        for cube in cubes:  
-            cube.coord('model_level_number').points = mls
-            mls = mls + len(cubes[1].coord('model_level_number').points)
-        cubes = cubes.concatenate()[0]
-   
+        ## find quanules
         def quantile_time(tstep):
             print(tstep)
-            return(cube[:,[tstep],:,:].collapsed('model_level_number', iris.analysis.PERCENTILE, percent = qs))
-        cube_quantile = [quantile_time(tstep) for tstep in range(cube.shape[1])]
+            out = cubes[:,[tstep],:,:].collapsed('model_level_number', iris.analysis.PERCENTILE, percent = qs)
+            return(out)
+        
+        cube_quantile = [quantile_time(tstep) for tstep in range(cubes.shape[1])]
         cube_quantile = iris.cube.CubeList(cube_quantile).concatenate()[0]
         iris.save(cube_quantile, outFile)
-        browser()
+        
 
 def run_for_period(period, experiment):
     print("experiment: " + experiment + "; period: " + period)
@@ -154,9 +181,12 @@ def run_for_period(period, experiment):
     sample_nos = range(0, params.shape[0], ngap)
     for i in sample_nos: runSample(i, output_dir_sample, params, input_data)
 
+    build_distribution_from_boots(output_dir_sample, output_dir_exp, "burnt_area_mode", period, "annual")
+    build_distribution_from_boots(output_dir_sample, output_dir_exp, "burnt_area_mode", period)
     if np.any(np.array(output) == 'Bootstraps'):
-        build_distribution_from_boots(output_dir_sample, output_dir_exp, 'bootstrap_', period)
-    browser()
+        build_distribution_from_boots(output_dir_sample, output_dir_exp, 'bootstrap_', period, "annual", transform = npLogistic)
+        build_distribution_from_boots(output_dir_sample, output_dir_exp, 'bootstrap_', period, transform = npLogistic)
+    
 
 for period in periods:
     for experiment in experiments:
