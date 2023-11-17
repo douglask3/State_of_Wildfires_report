b6d417342336528ad450c0c3c68699d9e767f3a6
Maria Barbosa <malucsp@gmail.com>
2023-11-17 09:42:49 -0300

    plotting relationship curves for the paper

--- test-code/response-curve-test.py
+++ test-code/response-curve-test.py
@@ -1,7 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def f(x, x0, p):
+
+plt.rcParams.update({'font.size': 14, 'font.family': 'Times New Roman'})
+
+'''
+#def f(x, x0, p):
     """
     Calculate f(x, x0, p) = ln|e^(x-x0)^p + 1|.
     
@@ -12,9 +16,9 @@ def f(x, x0, p):
 
     """
     
-    y = (np.exp(x - x0)) ** p
-    y = np.log(y + 1)
-    return y
+ #   y = (np.exp(x - x0)) ** p
+ #   y = np.log(y + 1)
+ #   return y
 
 #def linear_combined(x, beta, gama, x0, p):
     """
@@ -41,39 +45,45 @@ for x0, p in parameters:
     y_values = f(x_values,x0, p)
     #plt.plot(x_values, y_values, label=f"x0={x0}, p={p}")
     plt.plot(x_values, y_values)
- 
-plt.xlabel("x")
-plt.ylabel("Y")
+
+#plt.rcParams.update({'font.size': 14}) 
+plt.xlabel("Explanatory variables")
+plt.ylabel("Target variable")
 plt.legend()
 plt.grid(True)
 plt.show() 
     
 ''' 
-def linear_curve(a, b, x): 
-    return a*x + b
-#def power_response_curve(self, X):  
- #   return X**self
+
+
+#def linear_curve(a, b, x): 
+#    return a*x + b
+    
+def power_response_curve(X):  
+    return 2**X
 
 
 #def X2_response_curve(self, X):  
- #   return (X - self)**2.0
+#    return (X - self)**2.0
 
 x_values = np.linspace(0, 10, 1000)
 
 # Define different combinations self
-parameters = [(0.1, 0.5)]
+#parameters = [(0.1, 0.5)] #linear
+
+#parameters = [(0.5)] 
 
 # Plot the curves for different combinations of x0 and p
 #plt.figure(figsize=(10, 6))
-for a, b in parameters:
-    y_values = linear_curve(x_values,a,b)
+#for self in parameters:
+y_values = power_response_curve(x_values)
     #plt.plot(x_values, y_values, label=f"x0={x0}, p={p}")
-    plt.plot(x_values, y_values)
+plt.plot(x_values, y_values)
     
 
-plt.xlabel("x")
-plt.ylabel("Y")
+plt.xlabel("Explanatory variables")
+plt.ylabel("Target variable")
 plt.legend()
 plt.grid(True)
 plt.show()
-'''
+
