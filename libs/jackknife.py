e6fe952a528f510758fe3edb3c3f3601cf17846d
Maria Barbosa <malucsp@gmail.com>
2023-11-24 11:00:29 -0300

    Code running in Maria's note - no changes

--- libs/jackknife.py
+++ libs/jackknife.py
@@ -32,6 +32,7 @@ def jackknife(x_filen_list, X, fig_dir, **common_args):
     sorted_variable_names = list(sorted_contributions.keys())
     sorted_values = list(sorted_contributions.values())
     
+    print("Plotting Jackknife")
     plt.rcParams.update({'font.family': 'Times New Roman', 'font.size': 12})
 
     fig_jackknife = plt.figure(figsize=(10, 6))
@@ -40,6 +41,7 @@ def jackknife(x_filen_list, X, fig_dir, **common_args):
     # Using terracotta color (#c86558) for the bars
     bars = ax.barh(sorted_variable_names, sorted_values, color='#c86558', edgecolor='black', linewidth=0.8)
     
+    #adding values at the end of each bar
     for i, v in enumerate(sorted_values):
         ax.text(v + 0.1, i, str(round(v, 2)), color='black', va='center', fontsize=9)
     
@@ -48,15 +50,17 @@ def jackknife(x_filen_list, X, fig_dir, **common_args):
     ax.grid(axis='x', linestyle='-', linewidth=0.5, alpha=0.7)  # Adjusting grid appearance
     ax.invert_yaxis()
     
+    #removing borders
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
     ax.spines['bottom'].set_visible(False)
     ax.spines['left'].set_visible(False)
     
+    # Annotating arrow-like tips at the end of the left spine     
     ax.annotate('', xy=(0, 1.01), xytext=(0, -0.01), xycoords='axes fraction', textcoords='axes fraction',
             arrowprops=dict(arrowstyle='->', linewidth=1.5, color='black'))
             
-    # Annotating arrow-like tips at the end of the bottom spine facing towards the right
+    # Annotating arrow-like tips at the end of the bottom spine 
     ax.annotate('', xy=(1, 0), xytext=(-0.01, 0), xycoords='axes fraction', textcoords='axes fraction',
             arrowprops=dict(arrowstyle='->', linewidth=1.5, color='black'))
 
@@ -64,27 +68,12 @@ def jackknife(x_filen_list, X, fig_dir, **common_args):
     ax.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f'{x:.1f}'))
     ax.set_xticks(np.arange(0, max(sorted_values) + 1, 5))
     
-    plt.show()
-    set_trace()
+    #plt.show()
+    #set_trace()
     
-    '''
-    # Create the bar plot
-    print("Plotting Jackknife")
-    plt.rcParams.update({'font.family': 'Times New Roman', 'font.size': 12})
-    fig_jackknife = plt.figure(figsize=(10, 6))
     figure_filename = fig_dir +'jackknife'
     figure_dir =  combine_path_and_make_dir(figure_filename)
-    plt.barh(sorted_variable_names, sorted_values, color='#c86558')
-    plt.xlabel('Contribution (%)')
-    #plt.title('Variables Contribution')
-    plt.grid(axis='x', linestyle='--', alpha=0.6)
-    plt.gca().invert_yaxis()
     
-    plt.show()
-    set_trace()
-
     # Save the figure as a PNG file
     fig_jackknife.savefig(figure_filename + '.png')
     plt.clf()
-    color='#FF7F50'
-    '''
