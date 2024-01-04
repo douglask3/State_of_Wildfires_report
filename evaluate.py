0dae98e8576d7ebf1585644fe0393c4426315e90
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2024-01-04 12:12:03 +0000

    updating model evaluated

--- evaluate.py
+++ evaluate.py
@@ -299,7 +299,7 @@ if __name__=="__main__":
     """
     ### input data paths and filenames
 
-    training_namelist = "outputs//simple_example_model-speedtest///variables_info-Forest_consec_dry_mean_crop_pas_cveg-frac_points_0.005-nvariables_-frac_random_sample0.005-nvars_5-niterations_100.txt"
+    training_namelist = "outputs//simple_example_model-customPriors///variables_info-Forest_consec_dry_mean_tas_max_crop_pas_cveg-frac_points_0.005-nvariables_-frac_random_sample0.005-nvars_6-niterations_100.txt"
 
     config_namelist = "namelists/simple_example.txt"
 
