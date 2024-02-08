216ddea0e59dc141fd6a4aa0c50a1a1840ea2c86
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2024-02-08 15:24:51 +0000

    starting to make code that will otput and plot time series. Not quiet done yet

--- run_ConFire.py
+++ run_ConFire.py
@@ -4,6 +4,7 @@ from evaluate import *
 
 from   io     import StringIO
 import numpy  as np
+import cftime
 
 import matplotlib.pyplot as plt
 
@@ -14,6 +15,13 @@ if __name__=="__main__":
     
     trace, scalers, training_namelist = \
                     train_MaxEnt_model_from_namelist(namelist)
+
+    params = read_variables_from_namelist(training_namelist)
+    output_dir = params['dir_outputs']
+    output_file = params['filename_out']
+    control_direction = read_variables_from_namelist(params['other_params_file'])
+    control_direction = control_direction['control_Direction']
+    control_names = read_variables_from_namelist(namelist)['control_names']
 
     def call_eval(control_run_name, extra_params = None, run_only = True, *args, **kw):
         return evaluate_MaxEnt_model_from_namelist(training_namelist, namelist,
@@ -31,7 +39,7 @@ if __name__=="__main__":
     Control = call_eval('control', run_only = False)
     Standard = [Standard_limitation(i) for i in range(len(control_Direction))]
 
-    def make_time_series(cube):
+    def make_time_series(cube, name):
         try: 
             cube.coord('latitude').guess_bounds()
         except:
@@ -45,16 +53,27 @@ if __name__=="__main__":
         area_weighted_mean = cube.collapsed(['latitude', 'longitude'], 
                                             iris.analysis.MEAN, weights=grid_areas)
 
-        return area_weighted_mean.collapsed('realization', 
-                                            iris.analysis.PERCENTILE, percent=[10, 90])
-    
-    
-    control_TS = make_time_series(Control[0]).data
-    standard_TS = [make_time_series(cubes[0]).data for cubes in Standard]
+        TS = area_weighted_mean.collapsed('realization', 
+                                          iris.analysis.PERCENTILE, percent=[10, 90])
+        time_coord = TS.coord('time')
+        time_datetime = time_coord.units.num2date(time_coord.points)
+        time_datetime = cftime.date2num(time_datetime, 'days since 0001-01-01 00:00:00')/365.24
+        TS = np.append(time_datetime[:,None], np.transpose(TS.data), axis = 1)
 
-    colors = ['green', 'blue', 'red', 'black']  
+        out_file = figName + '/' + name + '.csv'
+        np.savetxt(out_file, TS, delimiter=',', header = "year,p10%,p90%")
+        return TS
+    
+    figName = output_dir + 'figs/' + output_file + '-control_TS'
+    makeDir(figName + '/')
+    control_TS = make_time_series(Control[0], 'Control')
+    standard_TS = np.array([make_time_series(cube[0], name) \
+                            for cube, name in zip(Standard, control_names)])
+    set_trace()
+    colors = [(0, 1, 0, 0.5), (0, 0, 1, 0.5), (1, 0, 0, 0.5), (0, 0, 0, 0.5)]  # Green, Blue, Red, Black with 50% opacity
+    
     fig, ax = plt.subplots()
-
+    
     for i in range(standard_TS.shape[0]):
         ax.fill_between(range(standard_TS.shape[2]), 
                         standard_TS[i, 0, :], standard_TS[i, 1, :], color=colors[i])
@@ -63,5 +82,5 @@ if __name__=="__main__":
     ax.set_xlabel('X-axis')
     ax.set_ylabel('Y-axis')
     ax.set_title('Polygons with Color Vision Impaired Color Combination')
-    set_trace()
+    
     
