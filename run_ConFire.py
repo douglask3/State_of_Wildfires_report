ff71b9f2bce8bccfe97ce27ccc6343d706bfe438
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2024-02-11 09:21:19 +0000

    setting up individual experiments from new sets of driving data

--- run_ConFire.py
+++ run_ConFire.py
@@ -10,24 +10,37 @@ import matplotlib.pyplot as plt
 
 
 if __name__=="__main__":
-    namelist = 'namelists/ConFire_example.txt'
+    namelist = 'namelists/ConFire_UK.txt'
     control_Direction = [1, -1, 1, -1]
     
     trace, scalers, training_namelist = \
                     train_MaxEnt_model_from_namelist(namelist)
 
     params = read_variables_from_namelist(training_namelist)
+    run_info = read_variables_from_namelist(namelist)
+
     output_dir = params['dir_outputs']
     output_file = params['filename_out']
+
     control_direction = read_variables_from_namelist(params['other_params_file'])
     control_direction = control_direction['control_Direction']
     control_names = read_variables_from_namelist(namelist)['control_names']
 
+    experiment_dirs  = run_info['experiment_dir']
+    experiment_names = run_info['experiment_names']
+    y_filen = run_info['x_filen_list'][0]
+    
     def call_eval(control_run_name, extra_params = None, run_only = True, *args, **kw):
         return evaluate_MaxEnt_model_from_namelist(training_namelist, namelist,
                                                     run_only = run_only, 
-                                                    control_run_name = 'Standard_fuel',
-                                                    extra_params = extra_params)
+                                                    control_run_name = control_run_name,
+                                                    extra_params = extra_params, *args, **kw)
+
+    Control = call_eval('control', run_only = False)
+    experiment = [call_eval(name, dir = dir, y_filen = y_filen) \
+                  for name, dir in zip(experiment_names, experiment_dirs)]
+    
+    
     def Standard_limitation(controlID):
         
         control_Directioni = control_Direction
@@ -36,7 +49,7 @@ if __name__=="__main__":
         
         return call_eval('Standard_'+ str(controlID), extra_params)
 
-    Control = call_eval('control', run_only = False)
+
     Standard = [Standard_limitation(i) for i in range(len(control_Direction))]
 
     def make_time_series(cube, name):
@@ -52,18 +65,19 @@ if __name__=="__main__":
         grid_areas = iris.analysis.cartography.area_weights(cube)
         area_weighted_mean = cube.collapsed(['latitude', 'longitude'], 
                                             iris.analysis.MEAN, weights=grid_areas)
+        area_weighted_mean = area_weighted_mean.aggregated_by('year', iris.analysis.MEAN)
         out_file = figName + '/points-' + name + '.csv'
         np.savetxt(out_file, area_weighted_mean.data, delimiter=',')
         
         TS = area_weighted_mean.collapsed('realization', 
-                                          iris.analysis.PERCENTILE, percent=[10, 90])
+                                          iris.analysis.PERCENTILE, percent=[25, 75])
         time_coord = TS.coord('time')
         time_datetime = time_coord.units.num2date(time_coord.points)
         time_datetime = cftime.date2num(time_datetime, 'days since 0001-01-01 00:00:00')/365.24
         TS = np.append(time_datetime[:,None], np.transpose(TS.data), axis = 1)
 
         out_file = figName + '/time_series' + name + '.csv'
-        np.savetxt(out_file, TS, delimiter=',', header = "year,p10%,p90%")
+        np.savetxt(out_file, TS, delimiter=',', header = "year,p25%,p75%")
         return TS
     
     figName = output_dir + 'figs/' + output_file + '-control_TS'
@@ -71,6 +85,9 @@ if __name__=="__main__":
     control_TS = make_time_series(Control[0], 'Control')
     standard_TS = np.array([make_time_series(cube[0], name) \
                             for cube, name in zip(Standard, control_names)])
+    experiment_TS = np.array([make_time_series(cube[0], name)  for cube, name in zip(experiment, experiment_names)])
+
+    
     set_trace()
     colors = [(0, 1, 0, 0.5), (0, 0, 1, 0.5), (1, 0, 0, 0.5), (0, 0, 0, 0.5)]  # Green, Blue, Red, Black with 50% opacity
     
