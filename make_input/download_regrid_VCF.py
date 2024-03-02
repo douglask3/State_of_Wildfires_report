6be42e0022ec5b00f1c3bb7d26a8aa29faa57994
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2024-03-02 10:00:21 +0000

    tidying

--- make_input/download_regrid_VCF.py
+++ make_input/download_regrid_VCF.py
@@ -22,7 +22,7 @@ lat_min, lat_max = 50.0, 60.0
 lon_min, lon_max = -11.0, 3.0
 
 temp_dir = '../temp'
-new_download = True
+new_download = False
 
 # Authenticate a session
 session = ModisSession(username=username, password=password)
@@ -43,17 +43,4 @@ if new_download:
     path = combine_path_and_make_dir(temp_dir, 'MODIS')
     GranuleHandler.download_from_granules(granules, session, path = "../temp/MODIS")
 
-'''
-# Define MODIS product URL and download the data
-url = "https://e4ftl01.cr.usgs.gov/MOLT/MOD44B.061/2017.03.06/MOD44B.A2017065.h17v03.061.2022284051332.hdf"
-response = requests.get(url)
-with open("MODIS_VCF.hdf", "wb") as f:
-    f.write(response.content)
 
-# Open MODIS HDF file
-modis_file = SD("MODIS_VCF.hdf", SDC.READ)
-
-# Read the necessary dataset
-dataset = modis_file.select("Majority_Land_Cover_Type_1")
-data = dataset.get()
-'''
