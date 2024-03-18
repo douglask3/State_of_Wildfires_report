6245cddb5f6cebdb948c923f86f3a76f788b9eae
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2024-03-18 12:35:41 +0000

    moving functions to above main

--- run_ConFire.py
+++ run_ConFire.py
@@ -8,10 +8,71 @@ import cftime
 
 import matplotlib.pyplot as plt
 
-
-if __name__=="__main__":
-    namelist = 'namelists/ConFire_Canada.txt'
+def call_eval(training_namelist, namelist,
+              control_run_name, extra_params = None, run_only = True, *args, **kw):
+    return evaluate_MaxEnt_model_from_namelist(training_namelist, namelist,
+                                               run_only = run_only, 
+                                               control_run_name = control_run_name,
+                                               extra_params = extra_params, *args, **kw)
     
+    
+def Standard_limitation(controlID, name, *args, **kws):   
+    control_Directioni = control_direction
+    control_Directioni[-controlID] = 0.0
+    extra_params = {"control_Direction": control_Directioni}
+        
+    return call_eval(name + 'Standard_'+ str(controlID), extra_params, *args, **kws)
+    
+def make_time_series(cube, name, figName):
+    try: 
+        cube.coord('latitude').guess_bounds()
+    except:
+        pass
+
+    try:
+        cube.coord('longitude').guess_bounds()
+    except:
+        pass
+    grid_areas = iris.analysis.cartography.area_weights(cube)
+    area_weighted_mean = cube.collapsed(['latitude', 'longitude'], 
+                                        iris.analysis.MEAN, weights=grid_areas)
+    area_weighted_mean = area_weighted_mean.aggregated_by('year', iris.analysis.MEAN)
+    out_file = figName + '/points-' + name + '.csv'
+    np.savetxt(out_file, area_weighted_mean.data, delimiter=',')
+    
+    TS = area_weighted_mean.collapsed('realization', 
+                                      iris.analysis.PERCENTILE, percent=[25, 75])
+    time_coord = TS.coord('time')
+    time_datetime = time_coord.units.num2date(time_coord.points)
+    time_datetime = cftime.date2num(time_datetime, 'days since 0001-01-01 00:00:00')/365.24
+    TS = np.append(time_datetime[:, None], np.transpose(TS.data), axis = 1)
+
+    out_file = figName + '/time_series' + name + '.csv'
+    np.savetxt(out_file, TS, delimiter=',', header = "year,p25%,p75%")
+    return TS
+    
+def run_experiment(training_namelist, namelist, name = '', *args, **kws):
+    if name != "": 
+        name = name + '-'
+        run_only = True
+    else:
+        run_only = False
+    Control = call_eval(training_namelist, namelist,
+                        name + 'control', run_only = run_only, *args, **kws)
+    
+    Standard = [Standard_limitation(i, name, *args, **kws) \
+                for i in range(len(control_direction))]
+
+    figName = output_dir + 'figs/' + output_file + '-' + name + 'control_TS'
+    makeDir(figName + '/')
+    control_TS = make_time_series(Control[0], 'Control', figName)
+    standard_TS = np.array([make_time_series(cube[0], name, figName) \
+                           for cube, name in zip(Standard, control_names)])
+    
+        
+    return Control, Standard, control_TS, standard_TS
+
+def run_ConFire(namelist):    
     trace, scalers, training_namelist = \
                     train_MaxEnt_model_from_namelist(namelist)
 
@@ -30,81 +91,22 @@ if __name__=="__main__":
     experiment_names = run_info['experiment_names']
     y_filen = run_info['x_filen_list'][0]
     
-    def call_eval(control_run_name, extra_params = None, run_only = True, *args, **kw):
-        return evaluate_MaxEnt_model_from_namelist(training_namelist, namelist,
-                                                    run_only = run_only, 
-                                                    control_run_name = control_run_name,
-                                                    extra_params = extra_params, *args, **kw)
-    
-    
-    def Standard_limitation(controlID, name, *args, **kws):
-        
-        control_Directioni = control_direction
-        control_Directioni[-controlID] = 0.0
-        extra_params = {"control_Direction": control_Directioni}
-        
-        return call_eval(name + 'Standard_'+ str(controlID), extra_params, *args, **kws)
-    
-    def make_time_series(cube, name, figName):
-        try: 
-            cube.coord('latitude').guess_bounds()
-        except:
-            pass
-
-        try:
-            cube.coord('longitude').guess_bounds()
-        except:
-            pass
-        grid_areas = iris.analysis.cartography.area_weights(cube)
-        area_weighted_mean = cube.collapsed(['latitude', 'longitude'], 
-                                            iris.analysis.MEAN, weights=grid_areas)
-        area_weighted_mean = area_weighted_mean.aggregated_by('year', iris.analysis.MEAN)
-        out_file = figName + '/points-' + name + '.csv'
-        np.savetxt(out_file, area_weighted_mean.data, delimiter=',')
-        
-        TS = area_weighted_mean.collapsed('realization', 
-                                          iris.analysis.PERCENTILE, percent=[25, 75])
-        time_coord = TS.coord('time')
-        time_datetime = time_coord.units.num2date(time_coord.points)
-        time_datetime = cftime.date2num(time_datetime, 'days since 0001-01-01 00:00:00')/365.24
-        TS = np.append(time_datetime[:, None], np.transpose(TS.data), axis = 1)
-
-        out_file = figName + '/time_series' + name + '.csv'
-        np.savetxt(out_file, TS, delimiter=',', header = "year,p25%,p75%")
-        return TS
-    
-    
-    def run_experiment(name = '', *args, **kws):
-        if name != "": 
-            name = name + '-'
-            run_only = True
-        else:
-            run_only = False
-        Control = call_eval(name + 'control', run_only = run_only, *args, **kws)
-        
-        Standard = [Standard_limitation(i, name, *args, **kws) \
-                    for i in range(len(control_direction))]
-
-        figName = output_dir + 'figs/' + output_file + '-' + name + 'control_TS'
-        makeDir(figName + '/')
-        control_TS = make_time_series(Control[0], 'Control', figName)
-        standard_TS = np.array([make_time_series(cube[0], name, figName) \
-                            for cube, name in zip(Standard, control_names)])
-    
-        
-        return Control, Standard, control_TS, standard_TS
-    
-    origonal = run_experiment()
+    origonal = run_experiment(training_namelist, namelist)
     
     experiment = [run_experiment(name, dir = dir, y_filen = y_filen) \
                   for name, dir in zip(experiment_names, experiment_dirs)]
-    
+
+
+if __name__=="__main__":
+    namelist = 'namelists/ConFire_Canada.txt'
+    run_ConFire(namelist)
+    set_trace()
     #experiment_TS = np.array([make_time_series(cube[0], name)  for cube, name in zip(experiment, experiment_names)])
     
     
 
     
-    set_trace()
+    
     colors = [(0, 1, 0, 0.5), (0, 0, 1, 0.5), (1, 0, 0, 0.5), (0, 0, 0, 0.5)]  # Green, Blue, Red, Black with 50% opacity
     
     fig, ax = plt.subplots()
