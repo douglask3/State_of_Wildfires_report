ae18d27b88f49c3d6be3902c3d0b03c3051384a1
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2024-04-26 12:10:27 +0100

    indenting properly

--- r-code/plot_ConFire_ts.r
+++ r-code/plot_ConFire_ts.r
@@ -1,31 +1,30 @@
 
 make_plot <- function() {
-openDat <- function(exp, nav = 0) {
-    dat = read.csv(paste0(fname, exp, fileEx))
+    openDat <- function(exp, nav = 0) {
+        dat = read.csv(paste0(fname, exp, fileEx))
+        
+        if (nav>0)  
+            dat[,2:3] = apply(dat[,2:3], 2, function(x) filter(x, rep(1/nav, nav), sides = 1))
+        #browser()
+            return(dat)
+    }      
+    dats = lapply(experiments, openDat, nav)
     
-    if (nav>0)  
-        dat[,2:3] = apply(dat[,2:3], 2, function(x) filter(x, rep(1/nav, nav), sides = 1))
-    #browser()
-    return(dat)
-}
-dats = lapply(experiments, openDat, nav)
+    
+    x_range = range(sapply(dats, function(i) i[,1]), na.rm = T)
+    y_range = range(sapply(dats, function(i) i[,-1]), na.rm = T)
+    
+    plot(x_range, y_range, type = 'n', xlab = '', ylab = '')
+    grid()
+    mtext(side = 2, 'burnt area (%/month)', line = 2)
+    #polygon(c(-9E9, 9E9)[c(1, 2, 2, 1)], c(-9E9, 9E9)[c(1, 1, 2, 2)], col = 'black')
 
-
-x_range = range(sapply(dats, function(i) i[,1]), na.rm = T)
-y_range = range(sapply(dats, function(i) i[,-1]), na.rm = T)
-
-plot(x_range, y_range, type = 'n', xlab = '', ylab = '')
-grid()
-mtext(side = 2, 'burnt area (%/month)', line = 2)
-#polygon(c(-9E9, 9E9)[c(1, 2, 2, 1)], c(-9E9, 9E9)[c(1, 1, 2, 2)], col = 'black')
-
-plot_exp <- function(dat, col) { 
-    polygon(c(dat[,1], rev(dat[,1])), c(dat[,2], rev(dat[,3])), 
-            border = NA, col = paste0(col, '44'))
-}
-
-
-for (i in 1:5) mapply(plot_exp, dats, cols)
+    plot_exp <- function(dat, col) { 
+        polygon(c(dat[,1], rev(dat[,1])), c(dat[,2], rev(dat[,3])), 
+                border = NA, col = paste0(col, '44'))
+    }
+    
+    for (i in 1:5) mapply(plot_exp, dats, cols)
 }
 
 png("r-code/ConFire_TS.png", height = 6, width = 6, res = 300, units = 'in')
