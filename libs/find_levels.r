e491c7f0eaec8ec8ca82cf95106b17c6be6c0060
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2024-05-07 14:41:45 +0100

    correcting sytmetric level clacliation

--- libs/find_levels.r
+++ libs/find_levels.r
@@ -16,15 +16,21 @@ find_levels <- function(z, percentiles = seq(20, 80, by = 20), not0 = FALSE) {
 }
 
 find_levels_n <- function(z, nlvls = 9, not0) {
+   
     if (is.raster(z)) z = as.numeric(z[])[!is.na(as.numeric(z[]))]
-    if (any(z < 0)) symetry = T else symetry = F
+    if (any(z < 0)) {
+        z = abs(z)
+        symetry = T 
+    } else symetry = F
     if (not0) z = z[z!=0]
     z = unique(z)
+    
     i = 0
     levels = c()
     sigfig = 0
 
-    if (symetry) nlvels = round(nlvels/2)
+    if (symetry) nlvls = ceiling(nlvls/2)
+     
     quants = head(seq(0, 1, length.out = nlvls + 2)[-1], -1)
     while (i < 20 && length(levels) < nlvls) {
         sigfig = sigfig + 1
@@ -33,6 +39,7 @@ find_levels_n <- function(z, nlvls = 9, not0) {
         levels = round(levels / scaling_factor, sigfig) * scaling_factor
         levels = unique(levels)
         print(levels)
+        i = i + 1
     }
     if (symetry) levels = c(-rev(levels), levels)
     return(levels)
