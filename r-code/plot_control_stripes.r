b1d919ce6eaff94cc8da0f1691340595913ab89e
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2024-05-09 07:07:42 +0100

    new run, plotting 2023 values as well as anaomoly.

--- r-code/plot_control_stripes.r
+++ r-code/plot_control_stripes.r
@@ -1,12 +1,15 @@
 graphics.off()
+library(stringr) 
+source("../LPX_equil/libs/legendColBar.r")
+source("libs/find_levels.r")
 
-dir = "outputs/ConFire_Canada-nrt7/figs/_22-frac_points_0.2-baseline-control_TS/"
+dir = "outputs/ConFire_Canada-nrt-tuning9/figs/_12-frac_points_0.5-baseline-control_TS/pc-95/"
 files = c("points-Control.csv",
-		  "points-potential-Fuel.csv",
-		  "points-potential-Moisture.csv",
-		  "points-potential-Weather.csv",
-		  "points-potential-Ignition.csv",
-		  "points-potential-Suppression.csv")
+	  "points-standard-Fuel.csv",
+	  "points-standard-Moisture.csv",
+	  "points-standard-Weather.csv",
+	  "points-standard-Ignition.csv",
+	  "points-standard-Suppression.csv")
 
 
 levels = c(0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5)
@@ -15,7 +18,7 @@ cols = list(c('#ffffcc','#ffeda0','#fed976','#feb24c','#fd8d3c','#fc4e2a','#e31a
 		    rev(c('#ffffd9','#edf8b1','#c7e9b4','#7fcdbb','#41b6c4','#1d91c0','#225ea8','#253494','#081d58', '#041034')),
 			c('#f7f4f9','#e7e1ef','#d4b9da','#c994c7','#df65b0','#e7298a','#ce1256','#980043','#67001f', '#340014'),
 			c('#fff5f0','#fee0d2','#fcbba1','#fc9272','#fb6a4a','#ef3b2c','#cb181d','#a50f15','#67000d', '#340202'),
-			c('#ffffff','#f0f0f0','#d9d9d9','#bdbdbd','#969696','#737373','#525252','#252525','#131313', '#000000'))
+			c('#fff5f0','#f0f0f0','#d9d9d9','#bdbdbd','#969696','#737373','#525252','#252525','#131313', '#340202'))
 
 dlevels = c(-0.05, -0.02, -0.01, -0.005, -0.002, -0.001, 0.001, 0.002, 0.005, 0.01, 0.02, 0.05)
 dcols = list(rev(c('#35000f', '#67001f','#b2182b','#d6604d','#f4a582','#fddbc7','#f0f0f0','#d1e5f0','#92c5de','#4393c3','#2166ac','#053061', '#031531')),
@@ -36,10 +39,10 @@ cut_results <- function(x, breaks) {
 split_to_day <- function(x) 
 	#approx(1:length(x), x, seq(1, length(x), by = 1/30))[[2]]
 	predict(smooth.spline(1:length(x), x), 
-		    seq(1, length(x), by = 1/30))[[2]]
+		    seq(0.5, length(x)+0.5, by = 1/30))[[2]]
 
 find_clim_av <- function(x) 
-	 sapply(1:360, function(day) mean(x[seq(day, length(x), by = 365)]))
+	 sapply(1:360, function(day) mean(x[seq(day, length(x), by = 360)]))
 
 
 
@@ -52,8 +55,8 @@ plot_strpes <- function(cdat, levels, cols) {
 		add_enemble <- function(ens) {
 			x = day + 0.5 * c(-1, 1)
 			y = c(ens, ens)
-			lines(x, y, col = cols[z[ens]], lwd = 8)
-			lines(x, -y, col = cols[z[ens]], lwd = 8)
+			lines(x, y, col = cols[z[ens]], lwd = 2)
+			lines(x, -y, col = cols[z[ens]], lwd = 2)
 		}
 		z = rev(sort(cdat[day,]))
 		lapply(1:ncol(cdat), add_enemble)
@@ -61,33 +64,62 @@ plot_strpes <- function(cdat, levels, cols) {
 
 	sapply(1:360, add_Day)
 	axis(at = seq(15, 360-15, 30), labels = month.abb, side = 1, pos = 0)
+	axis(at = c(-260, 700), labels = c('', ''), side = 1, pos = 0, xpd = FALSE)
 }
 
 
-plot_cols <- function(file, cols, levels, do_anom = FALSE) {
-        
+plot_cols <- function(file, cols, levels = NULL, do_last_year = FALSE, do_anom = FALSE) {   
 	dat = as.matrix(read.csv(paste0(dir, file), stringsAsFactors = FALSE))*100
 	idat = apply(dat, 1, split_to_day)
+        
 	clim = apply(idat, 2, find_clim_av)
+        
 	if (do_anom) {
-		clim = tail(idat, 360)-clim
-		levels = quantile(abs(clim), seq(0, 1, length.out = ceiling(length(cols))/2))[-1]
-		levels = c(rev(-levels), levels)
+	    if (file == files[1]) 
+                clim = tail(idat, 360) - clim
+            else
+                clim = log(tail(idat, 360)) - log(clim)
+	    #levels = quantile(abs(clim), seq(0, 1, length.out = ceiling(length(cols))/2))[-1]
+	    #levels = c(rev(-levels), levels)
+            extend_min = TRUE
+            minLab = ''
 	} else {
-		levels =  head(quantile(dat, seq(0, 1, length.out = length(cols) + 1))[-1], -1)
+            if (do_last_year) clim = tail(idat, 360)
+	    #levels =  head(quantile(dat, seq(0, 1, length.out = length(cols) + 1))[-1], -1)
+            extend_min = FALSE
+            minLab = 0
 	}
+        if (is.null(levels)) levels = find_levels_n(clim, 9, TRUE)
+        levels = unique(signif(levels), 2)
+        cols =  make_col_vector(cols, ncols = length(levels) + 1)
+	
 	plot_strpes(clim, levels, cols)
         if (file == files[1]) {
-            if (do_anom) mtext(side = 3, line = 1, 'Climatology')
-                else mtext(side = 3, line = 1, '2023')
+            if (do_anom) mtext(side = 3, line = 1, 'anomoly')
+                else if (do_last_year) mtext(side = 3, line = 1, '2023')
+                else mtext(side = 3, line = 1, 'Climatology')
         }
-        if (!do_anaom) 
-            mtext(side = 2, line = 1, file)
+        if (!do_anom && !do_last_year) {
+            if (grepl('Control', file)) {
+                txt = 'Burnt\nArea'
+            } else {
+                txt = gsub('-', ' ', gsub('.csv', '', gsub("points-", "", file)))
+                txt = str_to_title(txt)
+                txt = gsub(' ', '\n', txt)
+            }
+            mtext(side = 2, line = 1, txt)
+        }
+        legendColBar(c(365, 380), ncol(clim) * c(-0.9, 0.9), cols, levels, extend_max=TRUE, 
+                     extend_min = extend_min, minLab = minLab, add = TRUE, xtext_pos_scale= 1.4)
+        return(levels)
+        
 }
 
-png("figs/control_stripes.png", height = 6, width = 4, units = 'in', res = 300)
-par(mfcol = c(6, 2), mar = c(0, 1, 0, 1), oma = rep(2, 4))
+png("figs/control_stripes.png", height = 6.5, width = 10, units = 'in', res = 300)
+par(mfcol = c(6, 3), mar = c(0, 2, 0, 0), oma = c(0,2, 2, 3.5))
 
-mapply(plot_cols, files, cols, MoreArgs = list(levels))
-mapply(plot_cols, files, dcols, MoreArgs = list(dlevels, TRUE))
+levels = mapply(plot_cols, files, cols, SIMPLIFY = FALSE)
+
+mapply(plot_cols, files, cols, levels = levels, MoreArgs = list(do_last_year = TRUE))
+mapply(plot_cols, files, dcols, MoreArgs = list(do_anom = TRUE))
 dev.off()
