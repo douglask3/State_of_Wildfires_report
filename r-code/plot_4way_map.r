c0e7d329c06107c250891f4bc1133c3fc4c59374
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2024-05-16 06:58:58 +0100

    plotting uncertainty ranges via four maps, adding legend, and doingthis for 2023 abs. Andomolie (also calculated doifferent ebwteen mean clim at 2nd vs 1st half of the period

--- r-code/plot_4way_map.r
+++ r-code/plot_4way_map.r
@@ -8,11 +8,11 @@ sourceAllLibs("../ConFIRE_attribute/libs/")
 source("../ConFIRE_attribute/libs/plotStandardMap.r")
 source("../LPX_equil/libs/legendColBar.r")
 source("libs/find_levels.r")
+source("libs/number_to_hex.r")
 graphics.off()
 
 region = 'Greece'
 mnths = c(8)
-
 
 dir = paste0('outputs/ConFire_', region, '-nrt-tuning10/samples/_12-frac_points_0.5/baseline-/')
 
@@ -22,36 +22,20 @@ run_names = c('Burnt area' = 'control', 'Fuel' = 'Standard_0', 'Moisture' = 'Sta
 
 control_groups = list(1, 2, 3, 4, c(5, 6))
 
-levels = c(0.01, 0.05, 0.1, 0.5)
-nlevs = 5
-cols = c('#1b9e77','#d95f02','#7570b3')
+levels = c(0.01, 0.03, 0.1, 0.3)
+nlevs = 4
 cols = c('#00FF00', '#0000BB', '#FF0000')
-#cols = sapply(cols, function(col) make_col_vector(c(make_col_vector(c('black', col), ncols = 5)[2], col, 'white'), ncols = nlevs))
-#cols = sapply(cols, function(col) (make_col_vector(c(col, 'white'), ncols = nlevs)))
+
 
 make_col_map <- function(col1, col2) make_col_vector(c(col1, col2), ncols = 3)[2]
-# Function to convert a number to hexadecimal
-number_to_hex <- function(i, j = 1) {
-    
-    i = (i-1)/(nlevs-1)
-    j = (j-1)/(nlevs-1)
-    number =  (i+j)/2#sqrt(i^2 + j^2)
-    #number = min(i, j)
-    if (number > 1) number = 1
-    number = round(number*255)
-
-  if (number < 0 || number > 255) {
-    stop("Number must be between 0 and 255")
-  }
-  hex <- sprintf("%02X", number)
-  return(hex)
-}
-
 
 select_col <- function(i, j, k = 1, l = 1) 
     (c(paste0('#', number_to_hex(i, j), number_to_hex(j, k), number_to_hex(i,k)), l))
-    #c(make_col_map(make_col_map(cols[i, 1], cols[j, 2]), cols[k,3]), l)
 
+find_superLevel <- function(x) {
+    if (any(is.na(x))) return(NaN)
+    sum((x-1) * nlevs^(0:3)) + 1
+}
 
 open_runs  <- function(cntrl_ID) {
     runs = run_names[cntrl_ID]
@@ -63,9 +47,8 @@ open_runs  <- function(cntrl_ID) {
         
     files = sapply(runs, listFiles)
     
-    openFiles <- function(files) {
-        
-        tfile = paste(c('temp2/plot_nrt_map_4ways/mn-', mnths, '-', gsub('/', '-', files)),
+    openFiles <- function(files) {      
+        tfile = paste(c('temp2/plot_nrt_map_4ways/mn-', mnths, region, '-', gsub('/', '-', files)),
                       collapse = '-')
         
         if (file.exists(tfile)) return(brick(tfile))
@@ -96,13 +79,15 @@ BA = runs[[1]]
 
 controls = runs[-1]
 
-metric = 1
-get_control_range <- function(control, ctr,  metric) {
-    tfile = paste(c('temp2/plot_nrt_map_4ways/mn-', mnths, '-', region, run_names[ctr], 
+root_square_diff <- function(rs) sqrt(sum(rs**2))
+
+get_control_range <- function(control, ctr,  metric, FUN) {
+    tfile = paste(c('temp2/plot_nrt_map_4ways_qus/mn-', mnths, '-', region, run_names[ctr], 
                      metric, '.nc'),
                   collapse = '-')
     if (file.exists(tfile)) return(brick(tfile))
-    dats = layer.apply(control, function(i) i[[metric]])
+    
+    dats = layer.apply(control, function(i) FUN(i[[metric]]))
     for_quantile <- function(qu) {
         print(qu)
         calc(dats, function(x) quantile(x, qu,na.rm = TRUE))
@@ -112,41 +97,43 @@ get_control_range <- function(control, ctr,  metric) {
     out = writeRaster(out, file = tfile, overwrite = TRUE)
 }
 
-control_qu = mapply(get_control_range, controls, control_groups[-1], 1)
+control_qu = mapply(get_control_range, controls, control_groups[-1], 
+                    MoreArgs = list(1:2, root_square_diff))
+#control_qu = mapply(get_control_range, controls, control_groups[-1], 
+#                    MoreArgs = list(3:4, root_square_diff))
 
-map = addLayer(control_qu[[1]][[1]], control_qu[[2]][[2]], control_qu[[3]][[1]], control_qu[[4]][[1]])
+control_qu = mapply(get_control_range, controls, control_groups[-1], 
+                    MoreArgs = list(1, function(i) i))
 
-levels = quantile(map[], head(seq(0, 1, length.out = nlevs+1)[-1], -1), na.rm = TRUE)
+levels = find_levels_n(do.call(addLayer, control_qu), nlevs-1, TRUE)
 
-#map[[1]][] = 1
-#map[[2]][] = 1
-#map[[3]][] = 0
-#map[[4]][] = 1
+plot_uncertain_corners <- function(control_i) {
+    control_sample = rep(2, 4)
+    control_sample[control_i] = 1
+    map = do.call(addLayer, mapply(function(r, i) r[[i]], control_qu, control_sample))
+    #map = addLayer(control_qu[[1]][[1]], control_qu[[2]][[2]], control_qu[[3]][[1]], control_qu[[4]][[1]])
+    #browser()
+    #levels = quantile(map[], head(seq(0, 1, length.out = nlevs+1)[-1], -1), na.rm = TRUE)
+    cmap = cut_results(map, levels)
+    #browser()
+    cmap = calc(cmap, find_superLevel)
 
-cmap = cmap0 = cut_results(map, levels)
+    index = 1:nlevs
+    pcols = dots = c()
+    for (l in index) for (k in index) for (j in index) for (i in index) {
+        pcols = c(pcols, select_col(i,j,k,l)[1])
+        dots = c(dots, select_col(i,j,k,l)[2])
+    }
 
-find_superLevel <- function(x) {
-    if (any(is.na(x))) return(NaN)
-    #if (any(is.na(x))) browser()
-    #x[1] + sum((x-1) * (nlevs^(0:3)-1))
-    sum((x-1) * nlevs^(0:3)) + 1
+    plotStandardMap(cmap, pcols, limits = NULL, readyCut = TRUE)
+    pnts = cbind(xyFromCell(cmap, which(!is.na(cmap[]))), cmap[!is.na(cmap)])
+    #browser()
+    points(pnts[,1], pnts[,2], cex = (as.numeric(dots)[pnts[,3]]-1)/2, pch = 19, col = 'white')
 }
-cmap = calc(cmap, find_superLevel)
 
-plevels = pcols = c()
-index = 1:nlevs
-for (l in index) for (k in index) for (j in index) for (i in index) {   
-   plevels =  c(plevels, find_superLevel(c(i, j, k, l)))
-   pcols = c(pcols, select_col(i,j,k,l)[1])
-}
- plotStandardMap(cmap, pcols, limits = NULL, readyCut = TRUE)
-browser()
-#normalise_controls <- function(ens_id) {
-#    browser()
-#}
-
-#controls = lapply(1:length(controls[[1]]), normalise_controls)
-
+layout(rbind(1:2, 3:4, 5))
+par(mar = rep(0, 4))
+lapply(1:4, plot_uncertain_corners)
 
 
 leg_cube = 1:nlevs
@@ -155,17 +142,42 @@ for (i in 2:4)
     leg_cube = cbind(leg_cube, rep(leg_cube, each = 2^(i-1), , length.out = nlevs^4))
 
 
-
 plot_leg_square <- function(i, j, k = 1, l = 1) {
     col = select_col(i, j, k, l)
     x = i + (k - 1) * (nlevs + 1)
     y = j + (l - 1) * (nlevs + 1)
     polygon(x + c(-1, 0, 0, -1, -1), y + c(-1, -1, 0, 0, -1), col = col[1])
     points(x-0.5, y-0.5, col = 'white', pch = 19, cex = (as.numeric(col[2])-1)/4)
-    #browser()
+    
+    ascale = diff(par("usr")[1:2])
+    
+    if (j == 1) {
+        text(x-1, -0.06 * ascale, c(0, levels)[i], xpd = NA)
+        lines(c(x-1, x-1),c(0, -0.02*ascale))
+    }
+    if (i == 1) {
+        text(-0.03 * ascale, y-1, c(0, levels)[j], xpd = NA, adj = 1)
+        lines(c(0, -0.02*ascale), c(y-1, y-1))
+    }
+    if (i == 1 && j == 1 && l == 1) {
+        text(x-1, -0.18 * ascale, c(0, levels)[k], xpd = NA)
+        
+        lines(c(x-1, x + nlevs), rep(-0.12 *ascale, 2), xpd = NA)
+        lines(c(x-1, x-1), c(-0.12 *ascale, -0.14*ascale), xpd = NA)
+    }   
+    if (i == 1 && j == 1 && k == 1) {
+        text(-0.09 * ascale, y-1, c(0, levels)[l], xpd = TRUE, adj = 1)
+
+        lines(rep(-0.05 *ascale, 2), c(y-1, y + nlevs), xpd = NA)
+        lines(c(-0.05 *ascale, -0.07*ascale), c(y-1, y-1), xpd = NA)
+    }    
+    if (i == 1 && j == 1 && k == 1 && l == 1) {
+        text(nlevs/2, -0.09 * ascale, 'Fuel', xpd = T)
+        text(nlevs^2/2, -0.21 * ascale, 'Weather', xpd = T)
+    }
 }
-dev.new()
-plot(c(0, (nlevs+1)^2), c(0, (nlevs+1)^2), type = 'n', xlab = '', ylab = '')
+par(mar = c(6, 6, 0, 0))
+plot(c(0, (nlevs+1)^2), c(0, (nlevs+1)^2), type = 'n', xlab = '', ylab = '', axes = FALSE)
 
 lapply(1:nlevs, function(i) lapply(1:nlevs, function(j) 
        lapply(1:nlevs, function(k) lapply(1:nlevs, function(l) plot_leg_square(i,j, k, l)))))
