852283cdb4e9030c2273a509fbdea5db4a6b6082
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2024-05-28 21:09:26 +0100

    arread of increase/decrease and anao,liy and some tidying

--- r-code/plot_combinedControls.r
+++ r-code/plot_combinedControls.r
@@ -12,22 +12,17 @@ source("libs/number_to_hex.r")
 graphics.off()
 
 region = 'Greece'
-mnths = c(8)
-speedy = FALSE
-pltHeight = 0.5*8/7
+boxes = list(list(8, c(20, 27, 30.5, 42), 'Greece'))
 
 region = 'Canada'
-mnths = c(6)
-speedy = TRUE
-pltHeight = 0.33
-
-boxes = list(list(6, c(-80, -70, 47, 58)), list(5, c(-128, -105, 53, 62)),
-                  list(7, c(-80, -70, 47, 58)), list(9, c(-125, -115, 57, 64)))
+boxes = list(list(5, c(-123, -105, 53, 62), 'Western Shield'),
+             list(6, c(-80, -70, 47, 58), 'Quebec'), 
+             list(7, c(-80, -70, 47, 58), 'Quebec'), 
+             list(9, c(-124, -110, 57, 64), 'Western Shield'))
 
 #region = 'NW_Amazon'
-#mnths = c(10)
-#speedy = FALSE
-#pltHeight = 0.33
+#boxes = list(list( 9, c(-70, -55, -8, 0), 'Western Amazon'),
+#             list(10, c(-70, -55, -8, 0), 'Western Amazon'))
 
 dir = paste0('outputs/ConFire_', region, '-nrt-tuning10/samples/_12-frac_points_0.5/baseline-/')
 
@@ -42,12 +37,19 @@ nlevs = 6
 cols = c('#00FF00', '#0000BB', '#FF0000')
 
 
+boxes_from_raster_bool <- function(pnts, dx = 0.25, dy = dx, ...)
+                polygon(pnts[1] + dx * c(-1, 1, 1, -1, -1), 
+                pnts[2] + dy * c(-1, -1, 1, 1, -1),...)
+
 make_col_map <- function(col1, col2) make_col_vector(c(col1, col2), ncols = 3)[2]
 
 legLabs = list(c('Less Fuel', '', 'More Fuel'), 
                 c('Drier Fuel', '', 'Wetter Fuel'),
                 c('Low Fire Weather', '', 'High Fire Weather'),
                 c('Supression', '', 'Human-caused increase'))
+
+
+conf_levels = c(0.5, 0.75, 0.9, 0.95, 0.99)
 
 select_col <- function(i, j, k = 1, l = 1) {
     lab = mapply(function(x, y) x[y], head(legLabs, -1), c(i, j, k)) 
@@ -111,33 +113,38 @@ run_for_box <- function(box) {
         if (file.exists(tfile)) return(brick(tfile))
     
         dats = layer.apply(control, function(i) FUN(i[[metric]]))  
-        out = addLayer(mean(dats), mean(dats > 0))
+        qus = layer.apply(c(0.05, 0.95), function(qu) calc(dats, function(x) quantile(x, qu ,na.rm = TRUE)))
+        sig = mean(dats > 0)
+        out = qus[[1]]
+        out[sig > 0.5] = qus[[2]][sig > 0.5]
+        out = addLayer(out, sig)
         out = writeRaster(out, file = tfile, overwrite = TRUE)
     }
     
     control_increase = mapply(get_control_range, controls, control_groups[-1], 
                         MoreArgs = list(1:2, diff_))
 
-    plot_uncertain_corners <- function(cmap, dx = -0.25, dy = -0.25, cols, direction = 1) {
+    plot_uncertain_corners <- function(cmap, dx = -0.25, dy = -0.25, pcols, direction = 1) {
+        if (direction == 2) {
+            cmap[[1]] = - cmap[[1]]
+            cmap[[2]] = 1 - cmap[[2]]
+        }
         test = cmap[[1]] < 0
-        if (direction == 2) test = !test
+        #cmap0 = cmap
         cmap[test] = NaN
         cmap[[1]] = cut_results(cmap[[1]], levels)
-        cmap[[2]] = cut_results(cmap[[2]], c(0.01, 0.05, 0.1, 0.5, 0.9, 0.95, 0.99))/9
+        cmap[[2]] = cut_results(cmap[[2]], conf_levels)
+        #cmap[[2]] = length(conf_levels) + 1
+        cmap[[2]] = 0.25 + 0.75 * cmap[[2]]/(length(conf_levels)+1)
+        #cmap1 = cmap
+        cmap[[2]] = 14* cmap[[2]] / ncol(cmap)
         
-        pcols = make_col_vector(cols[[direction]], ncols = nlevs)
+        #pcols = make_col_vector(cols, ncols = nlevs)
         
         dat = cbind(xyFromCell(cmap, 1:length(cmap[[1]][])), cmap[[1]][], cmap[[2]][])
         dat = dat[!is.na(dat[,3]), ]
-        #cols = c(cols[1], 'white', cols[2])
-        #dat[,3] = 3
-        #plot_point <- function(pnts) {
-            #polygon(pnts[1] + c(0, dx, dx, 0, 0), 
-            #        pnts[2] + c(0, 0, dy, dy, 0), 
-            #        col = cols[pnts[3]], border = NA)
-        #}
-        #apply(dat, 1, plot_point)
         
+        points(dat[,1] + dx/2, dat[,2] + dy/2, pch = 19, col = 'black', cex = dat[,4])
         points(dat[,1] + dx/2, dat[,2] + dy/2, pch = 19, col = pcols[dat[,3]], cex = dat[,4])
         return()
     }
@@ -146,46 +153,105 @@ run_for_box <- function(box) {
     pltHeight = nrow(control_increase[[1]])/ncol(control_increase[[1]])
     
     levels = find_levels_n(abs(do.call(addLayer, control_increase)),  nlevs-1, TRUE) 
-    heights = c(pltHeight, 1, 1)  
-        
-    #png(paste(c("figs/limitation_map", region, '-', box[[1]], box[[2]], speedy, ".png"), 
-    #          collapse = '-'), 
-    #    height = 6*sum(heights), width = 6, units = 'in', res = 300)
-        layout(rbind(1, 2, 3), heights = heights)
-        par(mar = c(0, 1.5, 1.5, 0))
+    heights = c(0.25, pltHeight, 0.07, pltHeight, 0.6, 0.1)  
+    lmat = cbind(0, rbind(0, c(1, 0, 2), 0, c(3, 0, 4), 5, 0), 0)
+    widths = c(0.1, 1, 0.07, 1, 0.1)
+    png(paste(c("figs/limitation_map", region, '-', box[[1]], box[[2]], ".png"), 
+              collapse = '-'), 
+        height = 2.5*sum(heights), width = 2.5*sum(widths), units = 'in', res = 300)
+        layout(lmat, heights = heights, widths = widths)
+        par(mar = rep(0, 4))
         extent = extent(control_increase[[1]])
-        plot(extent[1:2], extent[3:4], xaxs = 'i', yaxs = 'i', type = 'n', xlab = '', ylab = '')
         
-# list(c('black', '#1b9e77', '#66c2a5'),
-#                    c('black', '#7570b3', '#8da0cb'),
-#                    c('black', '#d95f02', '#fc8d62'),
-#                    c('black', '#666666', '#DDDDDD')
-#list(c('black', '#009900', '#00ff00'), 
-#                   c('black', '#93A200', '#efff00'),
-#                   c('black', '#c65a00', '#ff8900'),
-#                   c('black', '#333333', '#AAAAAA'))
         
         obs = brick(paste0('data/data/driving_data/', region, '/nrt/period_2013_2023/burnt_area.nc'))
         obs = obs[[seq(box[[1]], nlayers(obs), by = 12)]]
         obs = crop(obs, box[[2]])
+        obs[[1]][is.na(control_increase[[1]][[1]])] = NaN
         cobs = obs[[nlayers(obs)]] > mean(obs[[1:(nlayers(obs)-1)]])
-        cobs[is.na(cobs)] = 0
-        coords = xyFromCell(cobs, (1:length(cobs)))
-        coords = coords[cobs[] == 1,] 
-    
-        boxFire <- function(pnts) polygon(pnts[1] + 0.25 * c(-1, 1, 1, -1, -1), 
-                                          pnts[2] + 0.25 * c(-1, -1, 1, 1, -1),
-                                          border = 'grey', col = '#FFFFDD', lty = 2)
-        apply(coords, 1, boxFire)
-        
+        pobs = 100*(obs[[nlayers(obs)]] - mean(obs[[1:(nlayers(obs)-1)]]))
 
-        mapply(plot_uncertain_corners, control_increase, c(-0.25, -0.25, 0.25, 0.25),
-                                                         c(-0.25, 0.25, -0.25, 0.25),
-              list(list(c('white', '#CCDDAA', '#225522'), c('white', '#EEEEBB', '#666633')),
-                   rev(list(c('white', '#EEEEBB', '#666633'), c('white', '#BBCCEE', '#222255'))),
-                   list(c('white', '#FFCCCC', '#663333'), c('white', '#CCEEFF', '#225555')),
-                   list(c('white', '#DDDDDD', '#555555')), list(c('white', '#DDDDDD', '#555555'))))
-        browser()
-    dev.off()
+        newPlot <- function() {
+            plot(extent[1:2], extent[3:4], xaxs = 'i', yaxs = 'i', 
+                 type = 'n', xlab = '', ylab = '', axes = FALSE)
+            contour(is.na(cobs), levels = 0.5, drawlabels=FALSE, add = TRUE)
+        }
+        newPlot()
+        
+            
+        levels_BA = find_levels_n(pobs, 9, TRUE)
+        cols_BA = rev(c('#b2182b','#d6604d','#f4a582','#fddbc7','#f7f7f7',
+                    '#d1e5f0','#92c5de','#4393c3','#2166ac'))
+        cols_BA = make_col_vector(cols_BA, ncols = length(levels_BA) +1)
+
+        addPoly <- function(pnts) {
+            polygon(pnts[1] + 0.25 * c(-1, 1, 1, -1, -1), pnts[2] + 0.25 * c(-1, -1, 1, 1, -1), col = cols_BA[pnts[3]], border = 'grey', lty = 2)
+        }
+        vobs = cut_results(pobs, levels_BA)
+        vobs = cbind(xyFromCell(vobs, 1:length(vobs)), vobs[])
+        vobs = vobs[!is.na(vobs[,3]),] 
+        apply(vobs, 1, addPoly)
+        contour(is.na(cobs), levels = 0.5, drawlabels=FALSE, add = TRUE)
+        axis(2)
+        axis(3)
+        lines(extent[c(1, 2, 2, 1, 1)], extent[c(3, 3, 4, 4, 3)])
+        mtext(side = 3, line = 2.2, 'Burnt area anomoly (%)')
+        legendColBar(c(0.95 - (0.10/pltHeight), 0.95), c(0, 1), 
+                     cols_BA, levels_BA, F, T, T, transpose = T, oneSideLabels = F)
+                
+        cols_controls = list(c('white', '#CCDDAA', '#225522'),
+                     c('white', '#BBCCEE', '#222255'),
+                     c('white', '#FFCCCC', '#663333'),
+                     c('white', '#CCEEFF', '#225555'))
+                
+        cols_controls = lapply(cols_controls, make_col_vector, ncols = length(levels) + 1)
+        
+        plot_direction <- function(direction = 1) {
+            newPlot()
+            title = paste0(c(c('increase', 'decrease')[direction], ' from controls'), collapse = '')
+            mtext(title, side = 1, line = 2.2)
+            if (direction == 2) cobs = 1 - cobs
+            cobs[is.na(cobs)] = -1
+            coords = xyFromCell(cobs, (1:length(cobs)))
+
+            apply(coords[cobs[] == 0,], 1, boxes_from_raster_bool, border = '#FFFFFF00', #density = 5 * ncol(cobs), 
+                   lty = 2, col = c('#EEEEEE', '#EEEEEE')[direction])
+            coords = coords[cobs[] == 1,] 
+            
+            apply(coords, 1, boxes_from_raster_bool, border = 'grey', #density = 5 * ncol(cobs), 
+                   lty = 2)
+            mapply(plot_uncertain_corners, control_increase, 
+                   c(-0.25, -0.25, 0.25, 0.25), c(-0.25, 0.25, -0.25, 0.25),
+                cols_controls, direction = direction)
+            axis(1)
+            axis(direction * 2)
+            lines(extent[c(1, 2, 2, 1, 1)], extent[c(3, 3, 4, 4, 3)])
+        }
+        lapply(1:2, plot_direction)
+        
+        
+        legX = seq(0.2, 0.7, length.out = length(cols_controls[[1]]))
+        addLegend <- function(y, cols, lab) {
+            for (col in list('black', cols)) 
+                points(legX, rep(y, length(legX)), col = col, pch = 19, cex = 2)
+            text(x = 0.05, y = y, adj = 1, lab) 
+    
+        }
+        
+        plot(c(-0.2, 1.2), c(0, 1.1), axes = FALSE, type = 'n', xlab = '', ylab = '')
+        mapply(addLegend, c(0.5, 0.35, 0.20, 0.05), cols_controls, 
+               c("Fuel", "Moisture", "Weather", "Human"))
+
+        text(c(legX[1], head(legX, -1) + diff(legX[1:2])/2, tail(legX, 1)), 
+             rep(0.6, length(legX) + 1), c('0', levels*100, '+'), srt = 30, adj = 0)
+        points(rep(0.85, length(conf_levels)), 
+               y = seq(0.05, 0.5, length.out = length(conf_levels)), 
+              pch = 19, cex = 2*(0.25 + 0.75*(1:length(conf_levels))/length(conf_levels)))
+        text(paste('>', conf_levels), x = 0.95, y = seq(0.05, 0.5, length.out = length(conf_levels)))
+        text('Confidence level', x = 0.9, y = 0.6)
+        
+        mtext(side = 3, outer = TRUE, font = 2,
+              line = -1.25, paste(box[[3]], '-', month.name[box[[1]]]))
+   dev.off()
 }
 lapply(boxes, run_for_box)
