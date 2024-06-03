e5e15985acaf9cc1b8345bb427ff461fb13b1d14
Douglas Kelley - els037 <douglas.i.kelley@gmail.com>
2024-06-03 13:20:02 +0100

    updating batch script

--- sbatch_metoffice.sh
+++ sbatch_metoffice.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
 #SBATCH --qos=long  #high #long
-#SBATCH --mem=50000  # 100000
-#SBATCH --ntasks-per-node=5            # Number of tasks per node
+#SBATCH --mem=100000  # 100000
+#SBATCH --ntasks-per-node=1            # Number of tasks per node
 #SBATCH --nodes=1                      # Number of nodes
 #SBATCH --output=outputs/output_%j.txt         # Output file (%j expands to job ID)
 #SBATCH --error=outputs/error_%j.txt           # Error file (%j expands to job ID)
-#SBATCH --time=14:00:00 
+#SBATCH --time=24:00:00 
 
 conda activate pymc5_env
 python run_ConFire.py
